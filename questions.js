9170e418da28ac87d9956a388bcc0cefe6224185
UserTestSchool <145926510+UserTestSchool@users.noreply.github.com>
2023-09-24 14:19:05 -0300

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,36 @@
+export default [
+  {
+    question: "Qual música é brasileira?",
+    answers: [
+      { option: "Drão", correct: true },
+      { option: "Bohemian Rhapsody", correct: false },
+      { option: "Imagine", correct: false },
+    ],
+  },
+  {
+    question: "Qual música é brasileira?",
+    answers: [
+      { option: "Shape of You", correct: false },
+      { option: "Eva", correct: true },
+      { option: "Billie Jean", correct: false },
+    ],
+  },
+  {
+    question: "Qual música é brasileira?",
+    answers: [
+      { option: "Outro", correct: false },
+      { option: "Celestial", correct: false },
+      { option: "Panis et Circencis", correct: true },
+    ],
+
+  },
+  {
+    question: "Qual música é brasileira?",
+    answers: [
+      { option: "Lemon", correct: false },
+      { option: "ADAMAS", correct: false },
+      { option: "Goodnight", correct: true },
+    ],
+
+  },
+];
